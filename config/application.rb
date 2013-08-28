0822db392c3acb79883d7757bf669f44eb3bf456
Kale Worsley <kale@worsley.co.nz>
2013-08-28 13:27:08 +1200

    Add default generator config.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,15 @@ module Rahani
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.view_specs false
+      g.helper_specs false
+      g.routing_specs false
+    end
   end
 end
