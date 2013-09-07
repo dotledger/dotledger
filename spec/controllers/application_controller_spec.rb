388f93288d147bbfdf6793ef4b4d5937396874c4
Kale Worsley <kale@worsley.co.nz>
2013-09-07 22:08:13 +1200

    Test application#boot.

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  describe 'GET boot' do
+    before { get :boot }
+
+    it { should respond_with :success }
+
+    it "should export some data" do
+      expect(controller.gon.all_variables.keys).to match_array [
+        "accounts",
+        "account_types",
+        "category_types"
+      ]
+    end
+  end
+end
