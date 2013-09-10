759955b2263dcffd9466845ee205551139c2721d
Kale Worsley <kale@worsley.co.nz>
2013-09-11 01:59:18 +1200

    Add `month_amount` to goal.

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -10,4 +10,15 @@ class Goal < ActiveRecord::Base
   validates :period, :presence => true, :inclusion => GOAL_PERIODS
 
   delegate :name, :type, :to => :category, :prefix => true
+
+  def month_amount
+    case self.period
+    when 'Month'
+      self.amount
+    when 'Fortnight'
+      self.amount * 26 / 12
+    when 'Week'
+      self.amount * 52 / 12
+    end
+  end
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -8,12 +8,32 @@ describe Goal do
   it { should have_db_column(:period).of_type(:string).with_options(:null => false, :default => 'Month') }
 
   it { should validate_presence_of :category }
-  
+
   it { should validate_presence_of :amount }
 
   it { should validate_presence_of :period }
-  
+
   it { should ensure_inclusion_of(:period).in_array(['Month', 'Fortnight', 'Week']) }
 
   it { should belong_to :category }
+
+  describe ".month_amount" do
+    context "month period" do
+      let(:goal) { FactoryGirl.create :goal, :amount => 1000.00, :period => 'Month' }
+
+      specify { expect(goal.month_amount).to be_within(0.1).of(1000.00) }
+    end
+
+    context "fortnight period" do
+      let(:goal) { FactoryGirl.create :goal, :amount => 1000.00, :period => 'Fortnight' }
+
+      specify { expect(goal.month_amount).to be_within(0.1).of(2166.67) }
+    end
+
+    context "week period" do
+      let(:goal) { FactoryGirl.create :goal, :amount => 1000.00, :period => 'Week' }
+
+      specify { expect(goal.month_amount).to be_within(0.1).of(4333.33) }
+    end
+  end
 end
