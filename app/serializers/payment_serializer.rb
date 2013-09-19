8b9671a3bcfeb2c95a05257a3ed1080b79535933
Kale Worsley <kale@worsley.co.nz>
2013-09-19 19:35:28 +1200

    Include payment occurences for the next 30 days in `upcoming`

--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -2,6 +2,6 @@ class PaymentSerializer < ActiveModel::Serializer
   attributes :id, :name, :amount, :category_id, :type, :schedule, :upcoming
 
   def upcoming
-    schedule.next_occurrences(5)
+    schedule.occurrences_between(Date.today, Date.today + 30.days)
   end
 end
