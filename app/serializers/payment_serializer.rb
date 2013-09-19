ae5a648176585fc61543b048702e685fc732e241
Kale Worsley <kale@worsley.co.nz>
2013-09-19 22:32:01 +1200

    Add `repeat`, `repeat_interval` , `repeat_period` and `date` to payment json.

--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -1,8 +1,37 @@
 class PaymentSerializer < ActiveModel::Serializer
   attributes :id, :name, :amount, :category_id, :category_name, :category_type,
-    :type, :schedule, :upcoming
+    :type, :schedule, :upcoming, :repeat, :repeat_interval, :repeat_period, :date
+
+  def repeat
+    schedule.recurrence_rules.length > 0
+  end
+
+  def repeat_interval
+    # FIXME: this is a bit yuck
+    first_rule.instance_variable_get(:@interval)
+  end
+
+  def repeat_period
+    case first_rule.class.to_s
+    when 'IceCube::DailyRule'
+      'Day'
+    when 'IceCube::WeeklyRule'
+      'Week'
+    when 'IceCube::MonthlyRule'
+      'Month'
+    end
+  end
+
+  def date
+    schedule.start_time.strftime('%Y-%m-%d')
+  end
 
   def upcoming
     schedule.occurrences_between(Date.today, Date.today + 30.days)
+  end
+
+  private
+  def first_rule
+    schedule.recurrence_rules.first
   end
 end
