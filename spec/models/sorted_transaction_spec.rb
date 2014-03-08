96e122322e6e85d81459750240c038cfb0559eb6
Kale Worsley <kale@worsley.co.nz>
2014-03-08 17:35:41 +1300

    Add some simple tag methods.

--- spec/models/sorted_transaction_spec.rb
+++ spec/models/sorted_transaction_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe SortedTransaction do
+
   it { should have_db_column(:account_id).of_type(:integer).with_options(:null => false) }
 
   it { should have_db_column(:transaction_id).of_type(:integer).with_options(:null => false) }
@@ -24,4 +25,50 @@ describe SortedTransaction do
   it { should belong_to :account }
 
   it { should belong_to :transaction }
+
+  describe "tags" do
+    let!(:tag1) { FactoryGirl.create :tag, :name => 'First Tag' }
+
+    let!(:tag2) { FactoryGirl.create :tag , :name => 'Second Tag'}
+
+    describe ".tags" do
+      subject { FactoryGirl.build :sorted_transaction, :tag_ids => [tag1.id, tag2.id] }
+
+      specify do
+        expect(subject.tags).to eq [tag1, tag2]
+      end
+    end
+
+    describe ".tags=" do
+      subject { FactoryGirl.build :sorted_transaction, :tag_ids => [] }
+
+      context "a list of tag models" do
+        before do
+          subject.tags = [tag1, tag2]
+        end
+
+        specify do
+          expect(subject.tag_ids).to eq [tag1.id, tag2.id]
+        end
+      end
+
+      context "a string of tag names" do
+        before do
+          subject.tags = 'First Tag, Second Tag'
+        end
+
+        specify do
+          expect(subject.tag_ids).to eq [tag1.id, tag2.id]
+        end
+      end
+
+      context "an unexpected type" do
+        specify do
+          expect {
+            subject.tags = {:foo => 'bar'}
+          }.to raise_error StandardError, 'unknown tag list'
+        end
+      end
+    end
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -10,4 +10,26 @@ describe Tag do
   it { should validate_presence_of :name }
 
   it { should validate_uniqueness_of :name }
+
+  describe "#tags_from_string" do
+    let(:tags_string) { 'First Tag, Second Tag' }
+
+    let(:tags_name_array) { ['First Tag', 'Second Tag'] }
+
+    context "existing tags" do
+      let!(:tag1) { FactoryGirl.create :tag, :name => 'First Tag' }
+
+      let!(:tag2) { FactoryGirl.create :tag , :name => 'Second Tag'}
+
+      specify do
+        expect(Tag.tags_from_string(tags_string)).to eq [tag1, tag2]
+      end
+    end
+
+    context "new tags" do
+      specify do
+        expect(Tag.tags_from_string(tags_string).map(&:name)).to eq tags_name_array
+      end
+    end
+  end
 end
