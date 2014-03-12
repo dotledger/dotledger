7e9956628521c88b534886a8976d9091943d4a44
Kale Worsley <kale@worsley.co.nz>
2014-03-12 21:09:22 +1300

    Fix deprecation warning.

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module DotLedger
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.i18n.enforce_available_locales = false
+
     config.generators do |g|
       g.stylesheets false
       g.helper false
