b1b805163467fe5228fa04d10c4cbec932b33da4
Kale Worsley <kale@worsley.co.nz>
2014-12-21 16:21:07 +1300

    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-require 'sprockets/railtie'
+require 'action_view/railtie'
+# require 'active_job/railtie'
+require 'active_model/railtie'
+require 'active_record/railtie'
 # require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -25,7 +28,8 @@ module DotLedger
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.i18n.enforce_available_locales = false
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
 
     config.generators do |g|
       g.stylesheets false
