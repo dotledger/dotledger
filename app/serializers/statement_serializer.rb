5dd2b30bcb1cc547ed5602223537093d396ff9fc
Kale Worsley <kale@worsley.co.nz>
2015-02-24 21:35:07 +1300

    Add created_at and transaction_count to statement serializer.

--- app/serializers/statement_serializer.rb
+++ app/serializers/statement_serializer.rb
@@ -1,3 +1,7 @@
 class StatementSerializer < ActiveModel::Serializer
-  attributes :id, :account_id, :balance, :from_date, :to_date
+  attributes :id, :account_id, :balance, :from_date, :to_date, :created_at, :transaction_count
+
+  def transaction_count
+    object.transactions.count
+  end
 end
