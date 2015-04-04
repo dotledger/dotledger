e59e5110acc6c94d561119fd74ff8ddad587c907
Kale Worsley <kale@worsley.co.nz>
2015-04-05 09:16:09 +1200

    Fix tag splitting regex.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   def self.tags_from_string(tag_list)
-    tag_list.split(/,\s+/).map do |tag_name|
+    tag_list.split(/,\s*/).map do |tag_name|
       Tag.where(name: tag_name.strip).first_or_create
     end
   end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -12,17 +12,19 @@ describe Tag do
   it { should validate_uniqueness_of :name }
 
   describe '#tags_from_string' do
-    let(:tags_string) { 'First Tag, Second Tag' }
+    let(:tags_string) { 'First Tag, Second Tag,Third Tag' }
 
-    let(:tags_name_array) { ['First Tag', 'Second Tag'] }
+    let(:tags_name_array) { ['First Tag', 'Second Tag', 'Third Tag'] }
 
     context 'existing tags' do
       let!(:tag1) { FactoryGirl.create :tag, name: 'First Tag' }
 
       let!(:tag2) { FactoryGirl.create :tag, name: 'Second Tag' }
 
+      let!(:tag3) { FactoryGirl.create :tag, name: 'Third Tag' }
+
       specify do
-        expect(described_class.tags_from_string(tags_string)).to eq [tag1, tag2]
+        expect(described_class.tags_from_string(tags_string)).to eq [tag1, tag2, tag3]
       end
     end
 
