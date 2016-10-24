3f739c976464fbff1891a6f131bb6d25447fad7f
Kale Worsley <kale@worsley.co.nz>
2016-10-24 22:51:32 +1300

    Add missing space in hash.

--- spec/services/balance_projector_spec.rb
+++ spec/services/balance_projector_spec.rb
@@ -19,26 +19,26 @@ describe BalanceProjector do
 
   let(:expected) do
     [
-      {balance: BigDecimal.new('1040.0'), date: Date.parse('2014-01-01')},
-      {balance: BigDecimal.new('1040.0'), date: Date.parse('2014-01-02')},
-      {balance: BigDecimal.new('1030.0'), date: Date.parse('2014-01-03')},
-      {balance: BigDecimal.new('1030.0'), date: Date.parse('2014-01-04')},
-      {balance: BigDecimal.new('1070.0'), date: Date.parse('2014-01-05')},
-      {balance: BigDecimal.new('1070.0'), date: Date.parse('2014-01-06')},
-      {balance: BigDecimal.new('1060.0'), date: Date.parse('2014-01-07')},
-      {balance: BigDecimal.new('1060.0'), date: Date.parse('2014-01-08')},
-      {balance: BigDecimal.new('1100.0'), date: Date.parse('2014-01-09')},
-      {balance: BigDecimal.new('1100.0'), date: Date.parse('2014-01-10')},
-      {balance: BigDecimal.new('1090.0'), date: Date.parse('2014-01-11')},
-      {balance: BigDecimal.new('1090.0'), date: Date.parse('2014-01-12')},
-      {balance: BigDecimal.new('1130.0'), date: Date.parse('2014-01-13')},
-      {balance: BigDecimal.new('1130.0'), date: Date.parse('2014-01-14')},
-      {balance: BigDecimal.new('1120.0'), date: Date.parse('2014-01-15')},
-      {balance: BigDecimal.new('1120.0'), date: Date.parse('2014-01-16')},
-      {balance: BigDecimal.new('1160.0'), date: Date.parse('2014-01-17')},
-      {balance: BigDecimal.new('1160.0'), date: Date.parse('2014-01-18')},
-      {balance: BigDecimal.new('1150.0'), date: Date.parse('2014-01-19')},
-      {balance: BigDecimal.new('1150.0'), date: Date.parse('2014-01-20')}
+      { balance: BigDecimal.new('1040.0'), date: Date.parse('2014-01-01') },
+      { balance: BigDecimal.new('1040.0'), date: Date.parse('2014-01-02') },
+      { balance: BigDecimal.new('1030.0'), date: Date.parse('2014-01-03') },
+      { balance: BigDecimal.new('1030.0'), date: Date.parse('2014-01-04') },
+      { balance: BigDecimal.new('1070.0'), date: Date.parse('2014-01-05') },
+      { balance: BigDecimal.new('1070.0'), date: Date.parse('2014-01-06') },
+      { balance: BigDecimal.new('1060.0'), date: Date.parse('2014-01-07') },
+      { balance: BigDecimal.new('1060.0'), date: Date.parse('2014-01-08') },
+      { balance: BigDecimal.new('1100.0'), date: Date.parse('2014-01-09') },
+      { balance: BigDecimal.new('1100.0'), date: Date.parse('2014-01-10') },
+      { balance: BigDecimal.new('1090.0'), date: Date.parse('2014-01-11') },
+      { balance: BigDecimal.new('1090.0'), date: Date.parse('2014-01-12') },
+      { balance: BigDecimal.new('1130.0'), date: Date.parse('2014-01-13') },
+      { balance: BigDecimal.new('1130.0'), date: Date.parse('2014-01-14') },
+      { balance: BigDecimal.new('1120.0'), date: Date.parse('2014-01-15') },
+      { balance: BigDecimal.new('1120.0'), date: Date.parse('2014-01-16') },
+      { balance: BigDecimal.new('1160.0'), date: Date.parse('2014-01-17') },
+      { balance: BigDecimal.new('1160.0'), date: Date.parse('2014-01-18') },
+      { balance: BigDecimal.new('1150.0'), date: Date.parse('2014-01-19') },
+      { balance: BigDecimal.new('1150.0'), date: Date.parse('2014-01-20') }
     ]
   end
 
