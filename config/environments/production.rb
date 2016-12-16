4a3e34849085d2748547a04b0e820e664fcb6bfd
Kale Worsley <kale@worsley.co.nz>
2016-12-17 08:49:52 +1300

    Add env var to enable logging to STDOUT.
    This comes from rails 5.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,12 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
